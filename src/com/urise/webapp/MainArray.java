6e9f82b002cdac37b45525055c1797277ee52b7d
Mikhail <pmixmix3@gmail.com>
2020-04-18 22:54:04 +0300

    делаю красивее

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -5,19 +5,21 @@ import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.ArrayStorage;
 
 public class MainArray {
+    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+
     public static void main(String[] args) {
-        ArrayStorage as = new ArrayStorage();
-        as.save(new Resume("M160361", "John", "Sins", "12.05.80", "Earth"));
 
-        System.out.println(as.get("M160361").toString());
+        ARRAY_STORAGE.save(new Resume("M160361", "John", "Sins", "12.05.80", "Earth"));
 
-        System.out.println("Кол-во анкет: " + as.size());
+        System.out.println(ARRAY_STORAGE.get("M160361").toString());
 
-        for (int i = 0; i < as.size(); i++) {
-            System.out.println(as.getAll()[i].toString());
+        System.out.println("Кол-во анкет: " + ARRAY_STORAGE.size());
+
+        for (int i = 0; i < ARRAY_STORAGE.size(); i++) {
+            System.out.println(ARRAY_STORAGE.getAll()[i].toString());
         }
-        as.clear();
-        System.out.println("Кол-во анкет: " + as.size());
+        ARRAY_STORAGE.clear();
+        System.out.println("Кол-во анкет: " + ARRAY_STORAGE.size());
 
     }
 }
